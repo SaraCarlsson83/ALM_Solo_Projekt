de97a4a9afe32733a4cba74e752e157ebe89afbe
Sara Carlsson <sara.carlsson@yh.nackademin.se>
2021-05-16 17:23:09 +0200

    added tests

---
+++ src/test/java/com/example/springdocker/model/MyMathCalcTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+/**
+*Created by Sara Carlsson
+*Date: 16/05/2021
+*Time:15:59
+*Project: spring-docker-demo
+*Copywright: MIT
+*/    
+class MyMathCalcTest {
+  
+}
---
+++ src/test/java/com/example/springdocker/repository/FoodRepositoryTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+/**
+*Created by Sara Carlsson
+*Date: 16/05/2021
+*Time:16:29
+*Project: spring-docker-demo
+*Copywright: MIT
+*/    
+class FoodRepositoryTest {
+  
+}
